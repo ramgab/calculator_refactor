7abfdafc041ff7cf5ede219cf3c800fdab3de71e
ramgab <90207508+ramgab@users.noreply.github.com>
2023-12-18 23:59:03 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,9 @@
+
+
+import java.util.Scanner;
+
 public class Calculator {
-    
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.performCalculation();
